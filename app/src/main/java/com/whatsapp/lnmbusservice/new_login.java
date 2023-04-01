034ef7d913381345ff467e0d8c54c558cdba0900
Ayush <ayushjaindeoli01@gmail.com>
2023-04-01 23:34:27 +0530

    Completed New Login Activity

--- app/src/main/java/com/whatsapp/lnmbusservice/new_login.java
+++ app/src/main/java/com/whatsapp/lnmbusservice/new_login.java
@@ -3,15 +3,12 @@ package com.whatsapp.lnmbusservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class new_login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_new_login);
     }
 }
