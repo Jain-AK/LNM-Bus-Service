089c5221947b4789f194c5a43e509298f864a159
Ayush <ayushjaindeoli01@gmail.com>
2023-04-15 01:39:47 +0530

    Adding user details to Firebase

--- app/src/main/java/com/whatsapp/lnmbusservice/user_detail.java
+++ app/src/main/java/com/whatsapp/lnmbusservice/user_detail.java
@@ -8,9 +8,15 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import static com.whatsapp.lnmbusservice.AdminLogin.ShowError;
 
@@ -21,6 +27,13 @@ public class user_detail extends AppCompatActivity {
     MaterialButton button;
 
     String userFirstName, userLastName, userBatch, userGender;
+
+    DatabaseReference databaseReference;
+
+    String mail;
+    String roll;
+
+    private FirebaseAuth firebaseAuth;
 
 
     @Override
@@ -48,39 +61,71 @@ public class user_detail extends AppCompatActivity {
 
         button = findViewById(R.id.btn_continue_details);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        mail = firebaseAuth.getCurrentUser().getEmail();
+
+        String[] split = mail.split("@");
+        roll = split[0];
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if (inputFirstName.getEditText() != null && inputLastName.getEditText() != null && inputGender.getEditText() != null) {
-                    userFirstName = inputFirstName.getEditText().getText().toString().toLowerCase();
-                    userLastName = inputLastName.getEditText().getText().toString().toLowerCase();
-                    userBatch = inputBatch.getEditText().getText().toString();
-                    userGender = inputGender.getEditText().getText().toString();
-                }
-
-                if (userFirstName.isEmpty()) {
-                    ShowError(inputFirstName, "Please Fill this field");
-                    return;
-                }
-                if (userLastName.isEmpty()) {
-                    ShowError(inputLastName, "Please Fill this field");
-                    return;
-                }
-                if (userGender.isEmpty()) {
-                    ShowError(inputGender, "Please Fill this field");
-                    return;
-                }
-                if (userBatch.isEmpty()) {
-                    ShowError(inputGender, "Please Fill this field");
-                    return;
-                }
-
-                startActivity(new Intent(user_detail.this, bookBus.class));
-                finish();
+                Insert();
 
             }
         });
+
+    }
+
+    private void Insert(){
+
+        if (inputFirstName.getEditText() != null && inputLastName.getEditText() != null && inputGender.getEditText() != null && inputBatch.getEditText() != null) {
+            userFirstName = inputFirstName.getEditText().getText().toString().toLowerCase();
+            userLastName = inputLastName.getEditText().getText().toString().toLowerCase();
+            userBatch = inputBatch.getEditText().getText().toString();
+            userGender = inputGender.getEditText().getText().toString();
+        }
+
+        if (userFirstName.isEmpty()) {
+            ShowError(inputFirstName, "Please Fill this field");
+            return;
+        }
+        if (userLastName.isEmpty()) {
+            ShowError(inputLastName, "Please Fill this field");
+            return;
+        }
+        if (userGender.isEmpty()) {
+            ShowError(inputGender, "Please Fill this field");
+            return;
+        }
+        if (userBatch.isEmpty()) {
+            ShowError(inputGender, "Please Fill this field");
+            return;
+        }
+
+        User user = new User(userFirstName, userLastName, userGender, userBatch);
+
+//        String email = firebaseAuth.getCurrentUser().getEmail();
+//        String email = databaseReference.push().getKey();
+
+        databaseReference.child("user")
+                .child(roll)
+                .setValue(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(user_detail.this, "Details Inserted Sucessfully", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        startActivity(new Intent(user_detail.this, bookBus.class));
+        finish();
 
     }
 }
